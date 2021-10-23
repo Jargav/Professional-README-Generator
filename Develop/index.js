8b605db16dec8cb763ed9b81f86a37b26734c532
Jared Gavigan <jagavigan80@gmail.com>
2021-10-22 20:33:23 -0400

    first commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        question: 'What is the name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        question: 'Describe your project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        question: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        question: 'Provide instructions and examples for use.',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        question: 'Please select the license you would like to choose:',
+        name: 'license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0']
+    },
+    {
+        type: 'input',
+        question: 'List your collaborators, if any, with links to their GitHub profiles.\nIf you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\nIf you followed tutorials, include links to those here as well.',
+        name: 'credits'
+    },
+    {
+        type: 'input',
+        question: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        question: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        question: 'What is your GitHub username?',
+        name: 'gitHubUsername'
+    },
+    {
+        type: 'input',
+        question: 'What is your email address?',
+        name: 'emailAddress'
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    let answers = await inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        var data = generateMarkdown(answers);
+        JSON.stringify(data)
+        writeToFile("generatedREADME.md", data);
+    })
+}
 
 // Function call to initialize app
 init();
